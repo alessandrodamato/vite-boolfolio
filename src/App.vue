a5dc1edd5200ff44780846c9ecbbd6c1a196fce3
alessandrodamato <alessandro042004@gmail.com>
2024-05-31 14:58:41 +0200

    [edit] scaffolding, [add] axios call in console.log

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,43 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+  import axios from 'axios';
+  import { store } from './data/store';
+  export default {
+
+    data(){
+      return{
+
+      }
+    },
+
+    methods:{
+      getApi(){
+        axios.get(store.apiUrl)
+             .then(res => {
+              console.log(res.data);
+             })
+             .catch(err => {
+              console.log(err.message);
+             })
+      }
+    },
+
+    mounted(){
+      this.getApi();
+    }
+
+  }
 </script>
 
+
+
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    test
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+<style lang="scss" scoped>
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
