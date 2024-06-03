a739fbbd3714a5966cc99f46e87053d3aedf8525
alessandrodamato <alessandro042004@gmail.com>
2024-06-03 15:46:15 +0200

    [edit] pages, [add] axios call to projects

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -7,13 +7,22 @@
 
 
 <template>
-  <div>
-
+  <div class="main-wrapper container">
+    <h1>Chi siamo</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam mollitia enim ducimus nisi veritatis, earum dicta eaque exercitationem accusamus a maiores architecto, dolore voluptas cumque veniam neque asperiores sequi consectetur voluptate similique quam officia hic? Voluptas veniam esse totam error id asperiores molestiae labore voluptate sit repellat minima, omnis adipisci veritatis hic consequuntur autem corporis, impedit odit quos, iste harum praesentium! Assumenda adipisci aut dolore autem delectus in architecto consectetur enim quibusdam similique tempora nulla voluptates cumque incidunt nesciunt laboriosam, dolorem expedita corporis est soluta voluptatum odit illo magnam ab. Laboriosam laborum harum, soluta a beatae vero debitis suscipit dicta.</p>
   </div>
 </template>
 
 
 
 <style lang="scss" scoped>
+
+  .main-wrapper{
+    padding: 50px;
+    text-align: center;
+    h1{
+      margin-bottom: 30px;
+    }
+  }
 
 </style>
--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -1,6 +1,45 @@
 <script>
+
+  import axios from 'axios';
+  import { store } from '../data/store';
+   import ProjectCard from '../components/partials/ProjectCard.vue';
+  import Paginator from '../components/partials/Paginator.vue';
+  import Loader from '../components/partials/Loader.vue';
+
   export default {
-    name: 'Projects'
+    name: 'Projects',
+    components:{
+      ProjectCard,
+      Paginator,
+      Loader
+    },
+    data(){
+      return{
+        projects: [],
+        links: [],
+        isLoading: true,
+        isError: false
+      }
+    },
+    methods:{
+      getApi(){
+        this.isLoading = true;
+        axios.get(store.apiUrl)
+             .then(res => {
+               this.projects = res.data.data;
+               this.links = res.data.links;
+               this.isLoading = false;
+             })
+             .catch(err => {
+              this.isLoading = false;
+              this.isError = true;
+              console.log(err.message);
+             })
+      }
+    },
+    mounted(){
+      this.getApi();
+    }
   }
 </script>
 
@@ -9,6 +48,22 @@
 <template>
   <div>
 
+    <div v-if="!isLoading && !isError" class="container projects-container">
+
+    <ProjectCard
+      v-for="project in projects"
+      :key="project.id"
+      :project="project"
+    />
+
+    </div>
+
+    <div v-if="!isLoading && !isError" class="container">
+    <Paginator :links="links" @changePage="getApi"/>
+    </div>
+
+    <Loader v-if="isLoading || isError"/>
+    
   </div>
 </template>
 
