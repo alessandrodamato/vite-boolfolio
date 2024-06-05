124612c7e590160a238472086a89b95255950eff
alessandrodamato <alessandro042004@gmail.com>
2024-06-05 15:54:38 +0200

    [add] form

--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -8,8 +8,17 @@
 
 <template>
   <div class="main-wrapper container">
-    <h1>Contatti</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam mollitia enim ducimus nisi veritatis, earum dicta eaque exercitationem accusamus a maiores architecto, dolore voluptas cumque veniam neque asperiores sequi consectetur voluptate similique quam officia hic? Voluptas veniam esse totam error id asperiores molestiae labore voluptate sit repellat minima, omnis adipisci veritatis hic consequuntur autem corporis, impedit odit quos, iste harum praesentium! Assumenda adipisci aut dolore autem delectus in architecto consectetur enim quibusdam similique tempora nulla voluptates cumque incidunt nesciunt laboriosam, dolorem expedita corporis est soluta voluptatum odit illo magnam ab. Laboriosam laborum harum, soluta a beatae vero debitis suscipit dicta.</p>
+    <form>
+      <div class="form-image"></div>
+      <div class="form-inputs">
+        <h2>Contattaci</h2>
+        <input type="text" placeholder="Nome">
+        <input type="text" placeholder="Cognome">
+        <input type="text" placeholder="Email">
+        <textarea rows="4" placeholder="Messaggio"></textarea>
+        <button type="submit">Invia email</button>
+      </div>
+    </form>
   </div>
 </template>
 
@@ -22,6 +31,54 @@
     text-align: center;
     h1{
       margin-bottom: 30px;
+    }
+    form{
+      display: flex;
+      justify-content: flex-end;
+      padding: 50px;
+      border-radius: 10px;
+      background-color: white;
+      .form-inputs, .form-image{
+        width: 50%;
+      }
+      .form-image{
+        background-image: url('https://mailtrap.io/wp-content/uploads/2019/09/How-to-Send-an-Email-Using-Windows-PowerShell_API_transparent.png');
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+      }
+      .form-inputs{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        padding: 0 30px;
+        input, textarea, button{
+          font-size: 1rem;
+          font-family: Arial, Helvetica, sans-serif;
+          width: 70%;
+          margin: 15px 0;
+          padding: 15px;
+          border-radius: 20px;
+          border: none;
+          outline: none;
+          background: linear-gradient(to bottom right, gray, lightgray);
+          &:hover{
+            background: linear-gradient(to top left, gray, lightgray);
+            cursor: pointer;
+          }
+          &::placeholder{
+            color: black;
+          }
+        }
+        button{
+          width: 30%;
+          background: linear-gradient(to bottom right, green, greenyellow);
+          &:hover{
+            background: linear-gradient(to top left, green, greenyellow);
+          }
+        }
+      }
     }
   }
 
